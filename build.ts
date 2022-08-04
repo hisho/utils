70f95652424ef3787bb21bd42af6d97c22942edf
hisho <hisho.web@gmail.com>
2022-08-04 17:00:28 +0900

    🤔 cjsとesmを配信するように変更

--- build.ts
+++ build.ts
@@ -1,14 +1,26 @@
 const { build } = require('esbuild')
 const { dependencies } = require('./package.json')
 
-build({
+const entryFile = 'src/index.ts'
+const shared = {
   bundle: true,
-  entryPoints: ['src/index.ts'],
+  entryPoints: [entryFile],
   external: Object.keys(dependencies ?? {}),
   logLevel: 'info',
   minify: true,
   sourcemap: false,
+}
+
+build({
+  ...shared,
   format: 'esm',
-  outfile: './dist/index.js',
+  outfile: './dist/index.esm.js',
+  target: ['ES6'],
+})
+
+build({
+  ...shared,
+  format: 'cjs',
+  outfile: './dist/index.cjs.js',
   target: ['ES6'],
 })
